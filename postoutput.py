6b04457fd06d0d6474e651a0b0e483f76154b628
HariR14 <67745254+HariR14@users.noreply.github.com>
2020-07-02 14:58:15 +0100

    Create postoutput.py

---
+++ postoutput.py
@@ -0,0 +1,3 @@
+import requests 
+url='https://lf8q0kx152.execute-api.us-east-2.amazonaws.com/default/computeFitnessScore' 
+x=requests.post(url,json={"qconfig":"4 7 3 0 6 1 5 2","userID":176766,"githubLink":"https://github.com/HariR14/8queens/blob/master/8queenscode.py"}) 
